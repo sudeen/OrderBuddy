cf72c48009f0404beb2bb300dc64ac3a625f1008
pacman <at4adim@gmail.com>
2017-11-07 22:02:05 +0545

    updated cors allowed origins

--- src/main/java/com/sudin/BuddyApplication.java
+++ src/main/java/com/sudin/BuddyApplication.java
@@ -25,7 +25,7 @@ public class BuddyApplication implements CommandLineRunner {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+                registry.addMapping("/**");
             }
         };
     }
